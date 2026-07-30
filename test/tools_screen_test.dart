5756ed79e630029cf298f2343555f16a05d3078c
towersystemofficial <towersystem.official@gmail.com>
2026-07-29 21:36:47 -0600

    Add prismatic glass surfaces

--- test/tools_screen_test.dart
+++ test/tools_screen_test.dart
@@ -4,6 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tower_lens/screens/tools_screen.dart';
 import 'package:tower_lens/services/library_service.dart';
 import 'package:tower_lens/services/text_ai_service.dart';
+import 'package:tower_lens/widgets/prismatic_surface.dart';
 
 void main() {
   testWidgets('launcher shows implemented tools and hides Appraiser',
@@ -43,18 +44,16 @@ void main() {
     );
     await tester.pumpAndSettle();
 
-    final featuredCard = tester.widget<Card>(
-      find.ancestor(
-        of: find.text('ToS Analysis'),
-        matching: find.byType(Card),
-      ),
+    final featuredCard = find.ancestor(
+      of: find.text('ToS Analysis'),
+      matching: find.byType(GlassCard),
     );
-    final ink = tester.widget<Ink>(
+    final box = tester.widget<SizedBox>(
       find.descendant(
-        of: find.byWidget(featuredCard),
-        matching: find.byType(Ink),
+        of: featuredCard,
+        matching: find.byType(SizedBox),
       ),
     );
-    expect(ink.height, 148);
+    expect(box.height, 148);
   });
 }
